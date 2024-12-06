5868299dbd861c160938818f243217488630c439
Dominik Šuráni <xsurani@stuba.sk>
2024-12-06 10:09:22 +0100

    v24.12.6_10:09 Add initial build configuration and log files for abb_moveit_config and zad3

---
+++ ros2_ws/build/abb_moveit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dominik/RRM/ros2_ws/src/zad3/abb_moveit_config
+# Build directory: /home/dominik/RRM/ros2_ws/build/abb_moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ ros2_ws/src/zad3/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dominik/RRM/ros2_ws/src/zad3/abb_moveit_config
+# Build directory: /home/dominik/RRM/ros2_ws/src/zad3/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
