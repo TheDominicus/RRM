c8531081c799df02d7abe4845e12b3bdc2362e51
Dominik Šuráni <xsurani@stuba.sk>
2024-12-06 11:00:34 +0100

    v24.12.6_11:00 New build, no changes

--- ros2_ws/src/zad3/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dominik/RRM/ros2_ws/src/zad3/abb_moveit_config
-# Build directory: /home/dominik/RRM/ros2_ws/src/zad3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
