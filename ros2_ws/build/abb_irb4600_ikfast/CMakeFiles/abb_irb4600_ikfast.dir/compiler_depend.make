e795891b909703cc1645f4856c321d8e9af8462a
Dominik Šuráni <xsurani@stuba.sk>
2024-12-06 10:20:18 +0100

    v24.12.6_10:20 Add AMENT_IGNORE files and update colcon build configurations for multiple packages

---
+++ ros2_ws/build/abb_irb4600_ikfast/CMakeFiles/abb_irb4600_ikfast.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for abb_irb4600_ikfast.
+# This may be replaced when dependencies are built.
--- ros2_ws/build/abb_moveit_config/colcon_command_prefix_build.sh
+++ ros2_ws/build/abb_moveit_config/colcon_command_prefix_build.sh
@@ -1,1 +1,2 @@
 # generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/dominik/RRM/ros2_ws/install/abb_model/share/abb_model/package.sh"
